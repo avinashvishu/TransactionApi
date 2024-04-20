440f22036e328143dddde4a8c9a8267a0d88101b
avinashvishu <loginavi1@gmail.com>
2024-04-21 01:10:09 +0530

    Statistic and Price Range done

--- controller/TransactionController.js
+++ controller/TransactionController.js
@@ -1,14 +1,108 @@
+const Product = require("../Model/Product");
+
 const AllTransaction = async (req, res) => {
   try {
     const response = req.body.paginatedResults.results;
-    console.log(response);
+    console.log(req.body.paginatedResults);
     if (response.length == 0) {
       return res.status(404).json({ message: "No transaction found" });
     }
-    res.status(200).json({ response, count: req.body.paginatedResults.count });
+    res.status(200).json({ response, result: req.body.paginatedResults });
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
-module.exports = { AllTransaction };
+
+const Statistics = async (req, res) => {
+  try {
+    const month = parseInt(req.query.month);
+
+    const TotalSaleAmount = await Product.aggregate([
+      {
+        $match: {
+          $expr: { $eq: [{ $month: "$dateOfSale" }, month] },
+          sold: true,
+        },
+      },
+      { $group: { _id: null, totalAmount: { $sum: "$price" } } },
+    ]);
+
+    const totalSoldItems = await Product.countDocuments({
+      $expr: { $eq: [{ $month: "$dateOfSale" }, month] },
+      sold: true,
+    });
+
+    const totalNotSoldItems = await Product.countDocuments({
+      $expr: { $eq: [{ $month: "$dateOfSale" }, month] },
+      sold: false,
+    });
+
+    const stat = {
+      totalSaleAmount:
+        TotalSaleAmount.length > 0 ? TotalSaleAmount[0].totalAmount : 0,
+      totalSoldItems: totalSoldItems,
+      totalNotSoldItems: totalNotSoldItems,
+    };
+    return stat;
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+const PriceRangeData = async (req, res) => {
+  try {
+    const month = parseInt(req.query.month);
+
+    const priceRanges = [
+      { range: "0 - 100", min: 0, max: 100 },
+      { range: "101 - 200", min: 101, max: 200 },
+      { range: "201 - 300", min: 201, max: 300 },
+      { range: "301 - 400", min: 301, max: 400 },
+      { range: "401 - 500", min: 401, max: 500 },
+      { range: "501 - 600", min: 501, max: 600 },
+      { range: "601 - 700", min: 601, max: 700 },
+      { range: "701 - 800", min: 701, max: 800 },
+      { range: "801 - 900", min: 801, max: 900 },
+      { range: "901 - above", min: 901, max: Infinity },
+    ];
+
+    const priceRangeCounts = {};
+
+    for (const range of priceRanges) {
+      const count = await Product.countDocuments({
+        $expr: {
+          $and: [
+            { $eq: [{ $month: "$dateOfSale" }, month] },
+            { $gte: ["$price", range.min] },
+            { $lte: ["$price", range.max] },
+          ],
+        },
+      });
+      priceRangeCounts[range.range] = count;
+    }
+    const totalCount = Object.values(priceRangeCounts).reduce(
+      (total, count) => total + count,
+      0
+    );
+
+    return { priceRangeCounts, totalCount };
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+const FetchAllData = async (req, res) => {
+  try {
+    const stats = await Statistics(req, res);
+    const priceRange = await PriceRangeData(req, res);
+    res.status(200).json({ stats, priceRange });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+module.exports = { AllTransaction, FetchAllData };
--- routes/TransactionRoutes.js
+++ routes/TransactionRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const { AllTransaction } = require("../controller/TransactionController");
+const {
+  AllTransaction,
+  FetchAllData,
+} = require("../controller/TransactionController");
 const Pagination = require("../Middleware/Pagination");
 const router = express.Router();
 
 router.get("/AllTrasnaction", Pagination, AllTransaction);
+router.get("/GetAllData", FetchAllData);
 module.exports = router;
