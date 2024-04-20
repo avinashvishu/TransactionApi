e945ad92fd15b81eb0db44c22d0ef54923f5ae49
avinashvishu <loginavi1@gmail.com>
2024-04-20 18:45:44 +0530

    added search query

--- Middleware/Pagination.js
+++ Middleware/Pagination.js
@@ -1,7 +1,8 @@
 const Product = require("../Model/Product");
+
 const Pagination = async (req, res, next) => {
   const page = parseInt(req.query.page);
-  const month = parseInt(req.query.month);
+  const month = req.query.month && parseInt(req.query.month);
   const searchQuery = req.query.search;
   const limit = 10;
 
@@ -9,24 +10,102 @@ const Pagination = async (req, res, next) => {
   const endIndex = page * limit;
 
   const results = {};
-
+  let count;
   if (searchQuery) {
     if (!isNaN(searchQuery)) {
+      count = await Product.aggregate([
+        {
+          $match: {
+            price: {
+              $gte: parseInt(searchQuery) - 1,
+              $lte: parseInt(searchQuery) + 1,
+            },
+          },
+        },
+        { $addFields: { month: { $month: "$dateOfSale" } } },
+        { $match: { month: month } },
+        { $project: { month: 0 } },
+      ]).exec();
+      if (endIndex < count.length) {
+        results.next = {
+          page: page + 1,
+          limit: limit,
+        };
+      }
+
+      if (startIndex > 0) {
+        results.previous = {
+          page: page - 1,
+          limit: limit,
+        };
+      }
+      try {
+        results.results = await Product.aggregate([
+          {
+            $match: {
+              price: {
+                $gte: parseInt(searchQuery) - 1,
+                $lte: parseInt(searchQuery) + 1,
+              },
+            },
+          },
+          { $addFields: { month: { $month: "$dateOfSale" } } },
+          { $match: { month: month || 3 } },
+          { $project: { month: 0 } },
+        ])
+          .limit(limit)
+          .skip(startIndex)
+          .exec();
+        req.body.paginatedResults = results;
+        next();
+      } catch (e) {
+        res.status(500).json({ message: e.message });
+      }
     } else {
-      filter.$or = [{ $text: { $search: searchQuery } }];
+      count = await Product.aggregate([
+        { $match: { $text: { $search: searchQuery } } },
+        { $addFields: { month: { $month: "$dateOfSale" } } },
+        { $match: { month: month } },
+        { $project: { month: 0 } },
+      ]).exec();
+      if (endIndex < count.length) {
+        results.next = {
+          page: page + 1,
+          limit: limit,
+        };
+      }
+
+      if (startIndex > 0) {
+        results.previous = {
+          page: page - 1,
+          limit: limit,
+        };
+      }
+      try {
+        results.results = await Product.aggregate([
+          { $match: { $text: { $search: searchQuery } } },
+          { $addFields: { month: { $month: "$dateOfSale" } } },
+          { $match: { month: month || 3 } },
+          { $project: { month: 0 } },
+        ])
+          .limit(limit)
+          .skip(startIndex)
+          .exec();
+        req.body.paginatedResults = results;
+        next();
+      } catch (e) {
+        res.status(500).json({ message: e.message });
+      }
     }
   }
 
-  if (
-    endIndex <
-    (await Product.aggregate([
-      { $addFields: { month: { $month: "$dateOfSale" } } },
-      { $match: { month: month } },
-      { $project: { month: 0 } },
-    ])
-      .countDocuments()
-      .exec())
-  ) {
+  //If user not provide any search queries
+  count = await Product.aggregate([
+    { $addFields: { month: { $month: "$dateOfSale" } } },
+    { $match: { month: month } },
+    { $project: { month: 0 } },
+  ]).exec();
+  if (endIndex < count.length) {
     results.next = {
       page: page + 1,
       limit: limit,
@@ -42,13 +121,13 @@ const Pagination = async (req, res, next) => {
   try {
     results.results = await Product.aggregate([
       { $addFields: { month: { $month: "$dateOfSale" } } },
-      { $match: { month: month } },
+      { $match: { month: month || 3 } },
       { $project: { month: 0 } },
     ])
       .limit(limit)
       .skip(startIndex)
       .exec();
-    res.paginatedResults = results;
+    req.body.paginatedResults = results;
     next();
   } catch (e) {
     res.status(500).json({ message: e.message });
--- controller/TransactionController.js
+++ controller/TransactionController.js
@@ -1,5 +1,11 @@
 const AllTransaction = async (req, res) => {
   try {
+    const response = req.body.paginatedResults.results;
+    console.log(response);
+    if (response.length == 0) {
+      return res.status(404).json({ message: "No transaction found" });
+    }
+    res.status(200).json({ response, count: req.body.paginatedResults.count });
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ error: "Internal server error" });
